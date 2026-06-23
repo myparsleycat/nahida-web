3eddbb968e71155db7e64e45c110d63a1ca40f5a
parsleycat <myparsleycat@gmail.com>
2026-06-23 12:58:26 +0900

    refactor(route): remove mobile-specific rendering logic from RouteComponent

--- src/routes/akasha/route.tsx
+++ src/routes/akasha/route.tsx
@@ -1,32 +1,12 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
-import { Center, Random1619 } from "@/components/common";
 import { NotiDialog } from "@/components/page/akasha/dialogs";
-import { useIsMobile } from "@/hooks/use-mobile";
 
 export const Route = createFileRoute("/akasha")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const isMobile = useIsMobile();
-
-  if (isMobile) {
-    return (
-      <Center className="p-4">
-        <div className="flex flex-col items-center space-y-4">
-          <Random1619 />
-          <div className="flex flex-col text-center">
-            <span className="text-lg">Please use a PC</span>
-            <span className="text-pretty text-muted-foreground">
-              Akasha services do not support mobile devices
-            </span>
-          </div>
-        </div>
-      </Center>
-    );
-  }
-
   return (
     <>
       <Outlet />
